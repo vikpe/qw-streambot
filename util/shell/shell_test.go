693d561f0987c19e3e1bd1adc30c6f353b3e0ff5
Viktor Persson <viktor.persson@arcsin.se>
2022-06-24 12:20:21 +0200

    Add test for shell utils.

---
+++ util/shell/shell_test.go
@@ -0,0 +1,22 @@
+package shell_test
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vikpe/streambot/util/shell"
+)
+
+func TestExecCommand(t *testing.T) {
+	t.Run("invalid cmd", func(t *testing.T) {
+		assert.Equal(t, "", shell.ExecCommand("__invalid_cmd__"))
+	})
+
+	t.Run("valid cmd", func(t *testing.T) {
+		nativeOutput, _ := exec.Command("ls").CombinedOutput()
+		expect := strings.TrimSpace(string(nativeOutput))
+		assert.Equal(t, expect, shell.ExecCommand("ls"))
+	})
+}
