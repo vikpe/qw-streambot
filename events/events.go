b10fad3126bc09d23b3c30b574cb20a862d04e3a
Viktor Persson <viktor.persson@arcsin.se>
2022-06-27 19:29:06 +0200

    Add events.

--- events/events.go
+++ events/events.go
@@ -2,10 +2,12 @@ package events
 
 const EzquakeProcessStart = "EZQUAKE_PROCESS_START"
 const EzquakeProcessStop = "EZQUAKE_PROCESS_STOP"
+const EzquakeServerConnect = "EZQUAKE_SERVER_CONNECT"
 
 const ServerMapChange = "SERVER_MAP_CHANGE"
-const ServerStatusChange = "SERVER_STATUS_CHANGE"
+const ServerStatusNameChange = "SERVER_STATUS_NAME_CHANGE"
 const ServerScoreChange = "SERVER_SCORE_CHANGE"
 const ServerTitleChange = "SERVER_TITLE_CHANGE"
 
-const StreambotHealthCheck = "STREAMBOT_HEALTH_CHECK"
+const StreambotHealthCheck = "STREAMBOT_HEALTHCHECK"
+const StreambotActionSuggestServer = "STREAMBOT_ACTION_SUGGEST_SERVER"
