6f5db41cc0e1a4e8a0b59eee91b742a11ec62ff3
Viktor Persson <viktor.persson@arcsin.se>
2022-06-28 09:36:19 +0200

    Add topics.

--- topics/topics.go
+++ topics/topics.go
@@ -11,5 +11,13 @@ const ServerScoreChange = "SERVER_SCORE_CHANGE"
 const ServerTitleChange = "SERVER_TITLE_CHANGE"
 
 const StreambotHealthCheck = "STREAMBOT_HEALTH_CHECK"
+const StreambotSystemUpdate = "STREAMBOT_SYSTEM_UPDATE"
 
 const ActionSuggestServer = "ACTION_SUGGEST_SERVER"
+
+const CommandConsole = "COMMAND_CONSOLE"
+const CommandCommand = "COMMAND_COMMAND"
+const CommandLastscores = "COMMAND_LASTSCORES"
+const CommandSay = "COMMAND_SAY"
+const CommandShowscores = "COMMAND_SHOWSCORES"
+const CommandTrack = "COMMAND_TRACK"
