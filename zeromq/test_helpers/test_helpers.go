fd4847ab3cddb3d573c5a2ac69fd95c9ec203f67
Viktor Persson <viktor.persson@arcsin.se>
2022-07-05 18:09:29 +0200

    Add zeromq test helpers.

---
+++ zeromq/test_helpers/test_helpers.go
@@ -0,0 +1,13 @@
+package test_helpers
+
+type PublisherMock struct {
+	SendMessageCalls [][]any
+}
+
+func NewPublisherMock() PublisherMock {
+	return PublisherMock{}
+}
+
+func (s *PublisherMock) SendMessage(topic string, args ...any) {
+	s.SendMessageCalls = append(s.SendMessageCalls, append([]any{topic}, args...))
+}
