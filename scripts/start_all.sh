faf40d919305162bd05a9bb6b905f78080970c29
Viktor Persson <viktor.persson@arcsin.se>
2022-07-06 16:28:35 +0200

    Update start all script.

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -3,7 +3,7 @@ echo "Starting all services"
 (
     trap 'kill 0' SIGINT
     bash scripts/proxy_controller.sh &
-    sleep 0.2s # wait for proxy
+    sleep 0.2 # wait for proxy
 
     bash scripts/brain_controller.sh &
     bash scripts/chatbot_controller.sh &
