2ddd06ca26c39ecb163976921f4a000da6405346
Viktor Persson <viktor.persson@arcsin.se>
2022-07-06 16:40:29 +0200

    Fix typo in scripts.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,12 +2,12 @@ echo "Starting all components"
 
 (
     trap 'kill 0' SIGINT
-    bash scripts/conrollers/proxy.sh &
+    bash scripts/controllers/proxy.sh &
     sleep 0.2 # wait for proxy
 
-    bash scripts/conrollers/brain.sh &
-    bash scripts/conrollers/chatbot.sh &
-    bash scripts/conrollers/ezquake.sh
+    bash scripts/controllers/brain.sh &
+    bash scripts/controllers/chatbot.sh &
+    bash scripts/controllers/ezquake.sh
 
     wait
 )
