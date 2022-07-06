c0f95fe92ed918ecb334481b59c88d071699a7ed
Viktor Persson <viktor.persson@arcsin.se>
2022-07-06 11:34:34 +0200

    Remove obsolete aliases.

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	ezquake2 "github.com/vikpe/streambot/pkg/ezquake"
-	zeromq2 "github.com/vikpe/streambot/pkg/zeromq"
+	"github.com/vikpe/streambot/pkg/ezquake"
+	"github.com/vikpe/streambot/pkg/zeromq"
 	"github.com/vikpe/streambot/third_party/twitch"
 )
 
@@ -13,15 +13,15 @@ func main() {
 	godotenv.Load()
 
 	playerName := os.Getenv("EZQUAKE_PLAYER_NAME")
-	process := ezquake2.NewProcess(os.Getenv("EZQUAKE_BIN_PATH"))
-	pipe := ezquake2.NewPipeWriter(os.Getenv("EZQUAKE_PROCESS_USERNAME"))
+	process := ezquake.NewProcessController(os.Getenv("EZQUAKE_BIN_PATH"))
+	pipe := ezquake.NewPipeWriter(os.Getenv("EZQUAKE_PROCESS_USERNAME"))
 	twitchClient := twitch.NewClient(
 		os.Getenv("TWITCH_BOT_CLIENT_ID"),
 		os.Getenv("TWITCH_CHANNEL_TITLE_ACCESS_TOKEN"),
 		os.Getenv("TWITCH_CHANNEL_BROADCASTER_ID"),
 	)
-	publisher := zeromq2.NewPublisher(os.Getenv("ZMQ_PUBLISHER_ADDRESS"))
-	subscriber := zeromq2.NewSubscriber(os.Getenv("ZMQ_SUBSCRIBER_ADDRESS"), zeromq2.TopicsAll)
+	publisher := zeromq.NewPublisher(os.Getenv("ZMQ_PUBLISHER_ADDRESS"))
+	subscriber := zeromq.NewSubscriber(os.Getenv("ZMQ_SUBSCRIBER_ADDRESS"), zeromq.TopicsAll)
 
 	bot := NewStreambot(
 		playerName,
