e45c88d99ea0c099b8c6df4440d716513e348637
Viktor Persson <viktor.persson@arcsin.se>
2022-07-07 13:49:40 +0200

    Move packages.

--- internal/brain/brain.go
+++ internal/brain/brain.go
@@ -15,13 +15,13 @@ import (
 	"github.com/vikpe/streambot/internal/brain/util/calc"
 	"github.com/vikpe/streambot/internal/brain/util/ezquake"
 	"github.com/vikpe/streambot/internal/brain/util/proc"
-	"github.com/vikpe/streambot/internal/brain/util/sstat"
 	"github.com/vikpe/streambot/internal/brain/util/task"
-	"github.com/vikpe/streambot/internal/brain/util/twitch"
 	"github.com/vikpe/streambot/internal/monitor"
 	"github.com/vikpe/streambot/pkg/zeromq"
 	"github.com/vikpe/streambot/pkg/zeromq/message"
 	"github.com/vikpe/streambot/third_party/qws"
+	"github.com/vikpe/streambot/third_party/sstat"
+	"github.com/vikpe/streambot/third_party/twitch"
 )
 
 var pfmt = prettyfmt.New("brain", color.FgHiMagenta, "15:04:05", color.FgWhite)
