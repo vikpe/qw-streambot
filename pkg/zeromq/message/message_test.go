2bf396c7fabcc20dc8825e80df85aaab39720ff8
Viktor Persson <viktor.persson@arcsin.se>
2022-07-08 22:17:40 +0200

    Refactor message test.

--- pkg/zeromq/message/message_test.go
+++ pkg/zeromq/message/message_test.go
@@ -31,36 +31,39 @@ func TestNewMessageFromParts(t *testing.T) {
 
 	t.Run("3 args", func(t *testing.T) {
 		t.Run("string", func(t *testing.T) {
+			serializedContent := message.Serialize("WORLD")
 			expect := message.Message{
 				Topic:       "HELLO",
-				Content:     message.SerializedValue(`"WORLD"`),
+				Content:     serializedContent,
 				ContentType: "string",
 			}
-			msg, err := message.NewMessageFromFrames([]string{"HELLO", "string", `"WORLD"`})
+			msg, err := message.NewMessageFromFrames([]string{"HELLO", "string", string(serializedContent)})
 			assert.Equal(t, expect, msg)
 			assert.Nil(t, err)
 			assert.Equal(t, "WORLD", msg.Content.ToString())
 		})
 
 		t.Run("int", func(t *testing.T) {
+			serializedContent := message.Serialize(33)
 			expect := message.Message{
 				Topic:       "HELLO",
-				Content:     message.SerializedValue("33"),
+				Content:     serializedContent,
 				ContentType: "int",
 			}
-			msg, err := message.NewMessageFromFrames([]string{"HELLO", "int", "33"})
+			msg, err := message.NewMessageFromFrames([]string{"HELLO", "int", string(serializedContent)})
 			assert.Equal(t, expect, msg)
 			assert.Nil(t, err)
 			assert.Equal(t, 33, msg.Content.ToInt())
 		})
 
-		t.Run("slice of strings", func(t *testing.T) {
+		t.Run("[]string", func(t *testing.T) {
+			serializedContent := message.Serialize([]string{"a", "b"})
 			expect := message.Message{
 				Topic:       "HELLO",
-				Content:     message.SerializedValue(`["a","b"]`),
+				Content:     serializedContent,
 				ContentType: "int",
 			}
-			msg, err := message.NewMessageFromFrames([]string{"HELLO", "int", `["a","b"]`})
+			msg, err := message.NewMessageFromFrames([]string{"HELLO", "int", string(serializedContent)})
 
 			assert.Equal(t, expect, msg)
 			assert.Nil(t, err)
