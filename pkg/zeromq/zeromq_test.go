336dd96cd3b8c1821a3a8eda2d725f1f8aaef160
Viktor Persson <viktor.persson@arcsin.se>
2022-07-08 20:02:38 +0200

    Update zeromq end to end test.

--- pkg/zeromq/zeromq_test.go
+++ pkg/zeromq/zeromq_test.go
@@ -10,9 +10,16 @@ import (
 )
 
 func TestEndToEnd(t *testing.T) {
-	wg := sync.WaitGroup{}
+	type outMessage struct {
+		Topic   string
+		Content any
+	}
 
-	topicsToSend := []string{"domain.topic1", "domain.topic2", "domain.topic3"}
+	messagesToSend := []outMessage{
+		{"domain.topic1", "hello world"},
+		{"domain.topic2", []string{"hello", "world"}},
+		{"domain.topic3", 666},
+	}
 
 	// proxy
 	go func() {
@@ -22,14 +29,15 @@ func TestEndToEnd(t *testing.T) {
 	zeromq.WaitForConnection()
 
 	// subscriber
-	topicsReceived := make([]string, 0)
+	wg := sync.WaitGroup{}
+	messagesRecieved := make([]message.Message, 0)
 
 	go func() {
 		subscriber := zeromq.NewSubscriber("tcp://localhost:5556", zeromq.TopicsAll)
-		subscriber.Start(func(message message.Message) {
-			topicsReceived = append(topicsReceived, message.Topic)
+		subscriber.Start(func(msg message.Message) {
+			messagesRecieved = append(messagesRecieved, msg)
 
-			if len(topicsReceived) == len(topicsToSend) {
+			if len(messagesRecieved) == len(messagesToSend) {
 				wg.Done()
 			}
 		})
@@ -40,8 +48,8 @@ func TestEndToEnd(t *testing.T) {
 	go func() {
 		publisher := zeromq.NewPublisher("tcp://localhost:5555")
 
-		for _, topic := range topicsToSend {
-			publisher.SendMessage(topic)
+		for _, msg := range messagesToSend {
+			publisher.SendMessage(msg.Topic, msg.Content)
 		}
 	}()
 
@@ -49,5 +57,19 @@ func TestEndToEnd(t *testing.T) {
 	wg.Wait()
 
 	// assertions
-	assert.Equal(t, topicsToSend, topicsReceived)
+	// message 1
+	assert.Equal(t, messagesToSend[0].Topic, messagesRecieved[0].Topic)
+	assert.Equal(t, messagesToSend[0].Content, messagesRecieved[0].Content.ToString())
+
+	// message 2
+	assert.Equal(t, messagesToSend[1].Topic, messagesRecieved[1].Topic)
+	var message2Content []string
+	messagesRecieved[1].Content.To(&message2Content)
+	assert.Equal(t, messagesToSend[1].Content, message2Content)
+
+	// message 3
+	assert.Equal(t, messagesToSend[2].Topic, messagesRecieved[2].Topic)
+	var message3Content int
+	messagesRecieved[2].Content.To(&message3Content)
+	assert.Equal(t, messagesToSend[2].Content, message3Content)
 }
