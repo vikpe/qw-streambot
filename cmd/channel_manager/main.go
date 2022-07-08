e346450b547c364054827611bc69a04fef9d3a6c
Viktor Persson <viktor.persson@arcsin.se>
2022-07-08 18:24:24 +0200

    Add error handler to channel manager.

--- cmd/channel_manager/main.go
+++ cmd/channel_manager/main.go
@@ -17,12 +17,17 @@ func main() {
 		return
 	}
 
-	manager := channel_manager.NewChannelManager(
+	manager, err := channel_manager.NewChannelManager(
 		os.Getenv("TWITCH_BOT_CLIENT_ID"),
 		os.Getenv("TWITCH_CHANNEL_TITLE_ACCESS_TOKEN"),
 		os.Getenv("TWITCH_CHANNEL_BROADCASTER_ID"),
 		os.Getenv("ZMQ_SUBSCRIBER_ADDRESS"),
 	)
+
+	if err != nil {
+		fmt.Println("unable to create channel manager:", err)
+		return
+	}
 
 	var pfmt = prettyfmt.New("channel_manager", color.FgHiRed, "15:04:05", color.FgWhite)
 	manager.OnStarted = func() {
@@ -30,6 +35,9 @@ func main() {
 	}
 	manager.OnStopped = func(signal os.Signal) {
 		pfmt.Println("stopped", signal)
+	}
+	manager.OnError = func(err error) {
+		pfmt.Println("error", err)
 	}
 
 	manager.Start()
