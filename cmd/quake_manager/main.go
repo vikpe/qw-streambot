469b87042eeafe5511cbc1b2b8a41bfef63f694b
Viktor Persson <viktor.persson@arcsin.se>
2023-08-31 16:54:36 +0200

    Refactor quake manager main.

--- cmd/quake_manager/main.go
+++ cmd/quake_manager/main.go
@@ -15,12 +15,12 @@ func main() {
 		return
 	}
 
-	brn := quake_manager.New(
+	manager := quake_manager.New(
 		os.Getenv("EZQUAKE_PLAYER_NAME"),
 		os.Getenv("EZQUAKE_BIN_PATH"),
 		os.Getenv("EZQUAKE_PROCESS_USERNAME"),
 		os.Getenv("ZMQ_PUBLISHER_ADDRESS"),
 		os.Getenv("ZMQ_SUBSCRIBER_ADDRESS"),
 	)
-	brn.Start()
+	manager.Start()
 }
